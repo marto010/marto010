07368c269d2e8a6a3cc418f26de287ca45d20d9c
marto010 <martinvenkov7@gmail.com>
2025-04-28 18:34:42 +0300

    Add files via upload

---
+++ beginnerproject3.py
@@ -0,0 +1,32 @@
+#Print the Sum of a Current Number and a Previous number
+previous_number = 0
+for i in range(10):
+   print(f"Current number: {i} Previous Number: {previous_number} Sum:{i+previous_number}")
+   previous_number +=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
