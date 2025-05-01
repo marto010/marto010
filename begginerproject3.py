b051030936ee4fd9da040e228fd6ed89956df789
marto010 <martinvenkov7@gmail.com>
2025-05-01 14:32:38 +0300

    Add files via upload

---
+++ begginerproject3.py
@@ -0,0 +1,63 @@
+#Python pattern
+for x in range(1,6):
+    for i in range(x):
+        print(x,end=" ")
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
