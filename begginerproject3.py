334a9d69c80d0dd0c7850accd00f70300123cf99
marto010 <martinvenkov7@gmail.com>
2025-05-01 14:43:54 +0300

    Add files via upload

--- begginerproject3.py
+++ begginerproject3.py
@@ -1,8 +1,17 @@
-#Python pattern
-for x in range(1,6):
-    for i in range(x):
-        print(x,end=" ")
-    print()
+#Merge two lists using the following condition
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
+result_list = []
+
+for x in list1:
+    if x % 2 != 0:
+       result_list.append(x)
+for i in list2:
+    if i % 2 == 0:
+        result_list.append(i)
+
+print(result_list)
+
 
 
 
